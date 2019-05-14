d0287fee93cfdfefe5fd45149319411638948ee3
zengqihao14 <zengqihao0104@gmail.com>
2019-05-14 10:40:46 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  div
+    nuxt
+</template>
+
+<style lang="sass">
+  @import '~@/assets/style/main'
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  section.container
+    div
+      logo
+      h1.title physic-demo
+      h2.sutitle physic-demo
+</template>
+
+<script>
+  export default {
+    components: {}
+  }
+</script>
